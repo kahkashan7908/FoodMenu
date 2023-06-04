3a477f52ba598a50a930108026a297ac37ced8a3
Kahkashan Khatoon <109336765+kahkashan7908@users.noreply.github.com>
2023-06-04 17:33:56 +0530

    Update views.py
    spelling corrections

--- userapp/views.py
+++ userapp/views.py
@@ -10,7 +10,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username=form.cleaned_data.get('username')
-            messages.success(request,f'welcom {username} your account is register')
+            messages.success(request,f'welcome {username} your account is register')
             return redirect('login')
     else:
         form=RegistrationForm()
